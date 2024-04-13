5ba62dc532d960d1387eeb6054dfdbf5c6748ef0
yumium <yuming.zai.00@gmail.com>
2024-04-13 15:44:50 +0100

    Added more tests

--- tests/test_orderbook.cpp
+++ tests/test_orderbook.cpp
@@ -1,21 +1,119 @@
-#include "../src/OrderBook.h"
+#include "../src/OrderBook.cpp"
 
 #include <gtest/gtest.h>
 
 TEST(TradeTest, TradeGetterTest) {
-  TradeInfo bidTrade = TradeInfo(0, 10, 10);
-  TradeInfo askTrade = TradeInfo(1, -10, 10);
+  TradeInfo bidTrade (0, 10, 10);
+  TradeInfo askTrade (1, -10, 10);
   Trade trade = Trade(bidTrade, askTrade);
-  EXPECT_EQ(trade.GetBidTrade(), askTrade);
+  EXPECT_EQ(trade.GetBidTrade(), bidTrade);
   EXPECT_EQ(trade.GetAskTrade(), askTrade);
 }
 
 TEST(OrderModifyTest, OrderModifyGetterTest) {
-  
+  OrderModify orderModify (1, Side::Buy, 10, 10);
+  EXPECT_EQ(orderModify.GetOrderId(), 1);
+  EXPECT_EQ(orderModify.GetSide(), Side::Buy);
+  EXPECT_EQ(orderModify.GetPrice(), 10);
+  EXPECT_EQ(orderModify.GetQuantity(), 10);
 }
 
 TEST(OrderModifyTest, OrderModifyOrderPointerTest) {
-  // stuff
+  OrderModify orderModify (1, Side::Buy, 10, 10);
+  std::shared_ptr<Order> p = orderModify.ToOrderPointer(OrderType::FillAndKill);
+  EXPECT_EQ(p->GetOrderId(), 1);
+  EXPECT_EQ(p->GetSide(), Side::Buy);
+  EXPECT_EQ(p->GetPrice(), 10);
+  EXPECT_EQ(p->GetInitialQuantity(), 10);
+  EXPECT_EQ(p->GetOrderType(), OrderType::FillAndKill);
+}
+
+TEST(OrderTest, OrderGetterTest) {
+  Order order (OrderType::FillAndKill, 1, Side::Buy, 10, 10);
+  EXPECT_EQ(order.GetOrderId(), 1);
+  EXPECT_EQ(order.GetSide(), Side::Buy);
+  EXPECT_EQ(order.GetPrice(), 10);
+  EXPECT_EQ(order.GetOrderType(), OrderType::FillAndKill);
+  EXPECT_EQ(order.GetInitialQuantity(), 10);
+  EXPECT_EQ(order.GetRemainingQuantity(), 10);
+  EXPECT_EQ(order.GetFilledQuantity(), 0);
+}
+
+TEST(OrderTest, OrderIsFilledTest) {
+  Order order (OrderType::FillAndKill, 1, Side::Buy, 10, 10);
+  order.Fill(10);
+  EXPECT_TRUE(order.isFilled());
+}
+
+TEST(OrderTest, OrderNotFilledTest) {
+  Order order (OrderType::FillAndKill, 1, Side::Buy, 10, 10);
+  order.Fill(5);
+  EXPECT_FALSE(order.isFilled());
+}
+
+TEST(OrderTest, OrderFillTest) {
+  Order order (OrderType::FillAndKill, 1, Side::Buy, 10, 10);
+  order.Fill(5);
+  EXPECT_EQ(order.GetRemainingQuantity(), 5);
+}
+
+TEST(OrderTest, OrderNotFillTest) {
+  Order order (OrderType::FillAndKill, 1, Side::Buy, 10, 10);
+  EXPECT_THROW(order.Fill(11), std::logic_error);
+}
+
+TEST(OrderBookTest, OrderBookAddOrderDuplicateTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  // The order below is dropped as the OrderID already exists
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Sell, 120, 10));
+  EXPECT_EQ(orderBook.Size(), 1);
+}
+
+TEST(OrderBookTest, OrderBookAddOrderTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 2, Side::Sell, 120, 10));
+  EXPECT_EQ(orderBook.Size(), 2);
+}
+
+TEST(OrderBookTest, OrderBookAddOrderFillAndKilTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 2, Side::Sell, 120, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::FillAndKill, 3, Side::Sell, 99, 12));
+  EXPECT_EQ(orderBook.Size(), 1);
+}
+
+TEST(OrderBookTest, OrderBookAddOrderGoodTillCancelTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 2, Side::Sell, 120, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 3, Side::Sell, 99, 12));
+  EXPECT_EQ(orderBook.Size(), 2);
+}
+
+TEST(OrderBookTest, OrderBookCancelOrderTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.CancelOrder(1);
+  EXPECT_EQ(orderBook.Size(), 0);
+}
+
+TEST(OrderBookTest, OrderBookCancelOrderFailTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.CancelOrder(2);
+  EXPECT_EQ(orderBook.Size(), 1);
+}
+
+TEST(OrderBookTest, OrderBookModifyOrderTest) {
+  Orderbook orderBook;
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 1, Side::Buy, 100, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 2, Side::Sell, 120, 10));
+  orderBook.AddOrder(std::make_shared<Order>(OrderType::GoodTillCancel, 3, Side::Sell, 110, 12));
+  orderBook.ModifyOrder(OrderModify (3, Side::Sell, 99, 12));
+  EXPECT_EQ(orderBook.Size(), 2);
 }
 
 int main(int argc, char **argv) {
